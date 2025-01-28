e7c5af8c14403802dcfae0decc1a1d988b8585e8
rmg <rmgajdosik@gmail.com>
2025-01-28 20:44:07 +0100

    Vytvorenie class Person a jej funkciu pozdrav.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Intro Object oriented programming
+class Person:
+    def __init__(self,name,age,gender,occupatiomn):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.occupation =occupatiomn
+    def pozdrav(self):
+        print(f"Ahoj volam sa {self.name} a mam {self.age} rokov")
+
+Roman = Person(name='Roman',age=39,gender='Male',occupatiomn='R.A.E')
+Roman.pozdrav()
